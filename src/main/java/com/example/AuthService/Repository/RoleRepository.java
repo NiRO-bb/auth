704837687c97b6c72fd6b8dba4b2e663d8388ae3
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-15 21:32:18 +0400

    feat: add repositories

---
+++ src/main/java/com/example/AuthService/Repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.AuthService.Repository;
+
+import com.example.AuthService.DTO.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
