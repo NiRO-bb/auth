e48ce0d65873786134185cf27bf1ffaee1add9a3
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-28 15:03:57 +0400

    feat: update Auth
    add signInOauth to AuthController
    add signInOauth and signUpOauth to AuthService
    add AuthUtil class
    add TokenWithUser class

--- src/main/java/com/example/AuthService/Controller/AuthController.java
+++ src/main/java/com/example/AuthService/Controller/AuthController.java
@@ -3,7 +3,9 @@ package com.example.AuthService.Controller;
 import com.example.AuthService.DTO.Token;
 import com.example.AuthService.DTO.User;
 import com.example.AuthService.Exception.DataNotFoundException;
+import com.example.AuthService.Exception.GoogleRegistrationMissingException;
 import com.example.AuthService.Service.AuthService;
+import com.example.AuthService.Utils.AuthUtil;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +34,7 @@ public class AuthController {
     private final static Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthService service;
+    private final AuthUtil util;
 
     /**
      * Calls service method to add new user to database.
@@ -53,6 +57,7 @@ public class AuthController {
     @PutMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody User user) {
         try {
+            user.setGoogleReg(false);
             User addedUser = service.signUp(user);
             LOGGER.info("User was added {}", user.desc());
             return new ResponseEntity<>(addedUser, HttpStatus.OK);
@@ -96,6 +101,36 @@ public class AuthController {
         } catch (Exception exception) {
             LOGGER.error("User authorization was failed; Error occurred {}", exception.getMessage());
             throw new RuntimeException("User authentification was failed.");
+        }
+    }
+
+    /**
+     * Authorizes users who was authenticated by OAuth2.
+     * Uses email value for identify user.
+     * Creates new user if passed email not exist in database.
+     *
+     * @return JWT with user authorities or JWT and user login and password - if new user was created
+     */
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User logged in",
+                    content = @Content(schema = @Schema(implementation = Token.class))),
+            @ApiResponse(responseCode = "404", description = "User authorization was failed - missing google registration",
+                    content = @Content(schema = @Schema(type = "string", example = "error message"))),
+            @ApiResponse(responseCode = "500", description = "User authorization was failed",
+                    content = @Content(schema = @Schema(type = "string", example = "error message")))
+    })
+    @GetMapping("/oauth")
+    public ResponseEntity<?> signInOauth() {
+        try {
+            Token token = service.signInOauth();
+            LOGGER.info("User logged in {}", String.format("{ \"email\":\"%s\" }", util.getEmail()));
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        } catch (GoogleRegistrationMissingException exception) {
+            LOGGER.error("User authorization was failed; Error occurred {}", exception.getMessage());
+            throw new GoogleRegistrationMissingException(exception.getMessage());
+        } catch (Exception exception) {
+            LOGGER.error("User authorization was failed; Error occurred {}", exception.getMessage());
+            throw new RuntimeException("User authentication was failed.");
         }
     }
 
