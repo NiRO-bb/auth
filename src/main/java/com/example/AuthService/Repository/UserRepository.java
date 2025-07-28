3fa8519d614cf6a36f9a7c1fe1395dcb438836ed
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-28 15:05:33 +0400

    fix: update User DTO
    add isGoogleReg field to User
    add findByEmail method to UserRepository

--- src/main/java/com/example/AuthService/Repository/UserRepository.java
+++ src/main/java/com/example/AuthService/Repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.example.AuthService.DTO.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
 
     boolean existsByLoginOrEmail(String login, String email);
 
